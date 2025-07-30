dadf0cc23bd9f94ffdf672d61ca954fdd2a2bda3
Brian Beatty <brian@27megahertz.com>
2025-07-30 08:12:13 -0500

    feat: reorg desktop base changes

--- build_files/07-base-image-changes.sh
+++ build_files/07-base-image-changes.sh
@@ -4,20 +4,4 @@ set ${SET_X:+-x} -eou pipefail
 
 echo "::group:: ===$(basename "$0")==="
 
-set -ouex pipefail
-
-rsync -rvK /ctx/etc /
-rsync -rvK /ctx/usr /
-
-# Test bluefin gschema override for errors. If there are no errors, proceed with compiling bluefin gschema, which includes setting overrides.
-mkdir -p /tmp/bluefin-schema-test
-find /usr/share/glib-2.0/schemas/ -type f ! -name "*.gschema.override" -exec cp {} /tmp/bluefin-schema-test/ \;
-cp /usr/share/glib-2.0/schemas/zz0-bluefin-modifications.gschema.override /tmp/bluefin-schema-test/
-cp /usr/share/glib-2.0/schemas/zz1-bluefin-modifications-mutter-exp-feats.gschema.override /tmp/bluefin-schema-test/
-echo "Running error test for bluefin gschema override. Aborting if failed."
-# We should ideally refactor this to handle multiple GNOME version schemas better
-glib-compile-schemas --strict /tmp/bluefin-schema-test
-echo "Compiling gschema to include bluefin setting overrides"
-glib-compile-schemas /usr/share/glib-2.0/schemas &>/dev/null
-
 echo "::endgroup::"
